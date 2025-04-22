c8eb2e2e32665f8ba2e35ff0bbdbdc69f9f80a39
Rapht <z5479419@ad.unsw.edu.au>
2025-04-22 20:37:56 +1000

    added route and file for edit game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import './index.css'
+import Dashboard from './Dashboard';
+import EditGame from './EditGame';
 import Login from './Login';
 import Register from './Register';
-import Dashboard from './Dashboard';
 
 function App() {
   const [token, setToken] = useState('');
@@ -14,9 +14,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={token ? <Navigate to="/dashboard" /> : <Login setfunction={setToken}/>} />
-        <Route path="/register" element={token ? <Navigate to="/dashboard" /> : <Register setfunction={setToken}/>} />
+        <Route path="/login" element={token ? <Navigate to="/dashboard" /> : <Login setfunction={setToken} />} />
+        <Route path="/register" element={token ? <Navigate to="/dashboard" /> : <Register setfunction={setToken} />} />
         <Route path="/dashboard" element={token ? <Dashboard setfunction={setToken} /> : <Navigate to="/login" />} />
+        <Route path="/game/:gameId" element={token ? <EditGame /> : <Navigate to="/login" />} />
         <Route path="*" element={<Navigate to={token ? "/dashboard" : "/login"} />} />
       </Routes>
     </Router>
---
+++ frontend/src/EditGame.jsx
@@ -0,0 +1,7 @@
+function EditGame() {
+  return (
+  <></>
+  );
+}
+
+export default EditGame;
